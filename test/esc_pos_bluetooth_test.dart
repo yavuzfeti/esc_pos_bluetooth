3e8bab44d2eb23cd8a171968df8322e612b908c6
Yavuz Feti <yavuzfeti@outlook.com>
2024-09-30 11:02:38 +0300

    Delete test directory

--- test/esc_pos_bluetooth_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:esc_pos_bluetooth/esc_pos_bluetooth_updated.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Tests not implemented', () {
-    expect(1, 1);
-  });
-}
